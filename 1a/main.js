db2748cc09e777c42ccb369b378a9305ecc94147
Stellan <Stellan.benckert@gmail.com>
2026-01-20 11:44:56 +0100

    circleArea imported to main

--- 1a/main.js
+++ 1a/main.js
@@ -2,10 +2,12 @@ import divide, { add as addition, subtract, multiply } from './math.js'
 
 import {PI, SECRET_SAUCE} from './math.js'
 
+import { circleArea } from './math.js';
 
 console.log(addition(10, 5));
 console.log(subtract(3, 5));
 console.log(multiply(4, 5));
-console.log(divide(9, 3))
+console.log(divide(9, 3));
+console.log(circleArea(4));
 
 console.log(PI, SECRET_SAUCE)
