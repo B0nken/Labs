5b1977559bdf5c0361921f757c7b867981d13205
Stellan <Stellan.benckert@gmail.com>
2026-01-20 11:33:03 +0100

    Lab 1-a Initial commit with math functions

---
+++ 1a/main.js
@@ -0,0 +1,11 @@
+import divide, { add as addition, subtract, multiply } from './math.js'
+
+import {PI, SECRET_SAUCE} from './math.js'
+
+
+console.log(addition(10, 5));
+console.log(subtract(3, 5));
+console.log(multiply(4, 5));
+console.log(divide(9, 3))
+
+console.log(PI, SECRET_SAUCE)
