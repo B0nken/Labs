fd088837bd5c3af51e00f782b9e49e19caaaeb16
Stellan <Stellan.benckert@gmail.com>
2026-01-31 11:58:07 +0100

    new js labs

--- 2b/main.js
+++ 2b/main.js
@@ -14,3 +14,49 @@ const person = {
 console.log(person.getFullName());
 person.introduce();
 
+
+
+const car = {
+    brand: "volvo",
+    model: "v40",
+    year: 2018,
+
+    getDescription() {
+        return `${this.year} ${this.brand} ${this.model} `;
+    },
+
+    honk() {
+        console.log("tut tut!");
+    }
+};
+
+console.log(car.getDescription());
+car.honk();
+
+
+const counter = {
+    value: 0,
+
+    increment() {
+        this.value++;
+    },
+
+    decrement() {
+        this.value--;
+    },
+
+    reset() {
+        this.value = 0;
+    }
+};
+
+console.log("värdet initialt: ", counter.value);
+counter.increment();
+console.log("värde efter ökning ", counter.value)
+counter.increment();
+console.log("värde efter ännu en ökning", counter.value);
+
+counter.decrement();
+console.log("värde efter minskning", counter.value);
+counter.reset();
+console.log("värde efter återställning", counter.value);
